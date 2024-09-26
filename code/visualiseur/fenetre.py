6ed4dc6a3d0df65ee6fee6d82da9a5b80bbff231
Lyune <laurency.yuna@gmail.com>
2024-09-26 15:51:07 +0200

    Ajout afficher quadrillage

--- code/visualiseur/fenetre.py
+++ code/visualiseur/fenetre.py
@@ -9,7 +9,12 @@ class Fenetre:
         self.titre = titre
         self.ecran = pygame.display.set_mode((self.largeur, self.hauteur))
         pygame.display.set_caption(self.titre)
-        self.ecran.fill((255,255,255))
+
+    def afficher_quadrillage(self, taille_case, couleur):
+        for x in range(0, self.largeur, taille_case):
+            pygame.draw.line(self.ecran, couleur, (x, 0), (x, self.hauteur))
+        for y in range(0, self.hauteur, taille_case):
+            pygame.draw.line(self.ecran, couleur, (0, y), (self.largeur, y))
 
     def afficher(self):
         ouvert = True
@@ -17,5 +22,7 @@ class Fenetre:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     ouvert = False
+            self.ecran.fill((255,255,255))
+            self.afficher_quadrillage(10, (200,200,200))
             pygame.display.flip()
         pygame.quit()
