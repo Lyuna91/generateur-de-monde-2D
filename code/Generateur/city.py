2036b5011a14fd1e82bb7d6e99cb9f9294d5ef68
Lyune <laurency.yuna@gmail.com>
2024-10-23 20:03:28 +0200

    Mise en forme selon la doc technique : City et Country

--- code/Generateur/city.py
+++ code/Generateur/city.py
@@ -1,10 +1,30 @@
 from .country import Country
+
 class City:
+
+######################### INIT
+
     def __init__(self, id, name, position, country):
+        """
+        Initialise une ville avec ses caractéristiques de base.
+
+        :param id: Identifiant unique de la ville
+        :param name: Nom de la ville
+        :param position: Position de la ville sur la carte
+        :param country: Pays auquel la ville appartient
+        """
         self.id = id
         self.name = name
         self.position = position
         self.country = country  
+
+    def __repr__(self):
+        """
+        Retourne une représentation de la ville sous forme de string.
+        """
+        return f"City(id={self.id}, name={self.name}, position={self.position}, country={self.country})"
+
+######################### METHODS
 
     def create_city(self):
         pass
--- code/Generateur/country.py
+++ code/Generateur/country.py
@@ -1,6 +1,25 @@
 from .zone import Zone
+
 class Country:
+
+######################### INIT
+
     def __init__(self, id, name, size, zones):
+        """
+        Initialise un pays avec ses caractéristiques de base.
+        
+        :param id: Identifiant unique du pays
+        :param name: Nom du pays
+        :param zones: Liste des zones du pays
+        """
         self.id = id
         self.name = name
-        self.zones = zones # Liste des zones 
+        self.zones = zones
+
+    def __repr__(self):
+        """
+        Retourne une représentation du pays sous forme de string.
+        """
+        return f"Country(id={self.id}, name={self.name}, zones={self.zones})"
+    
+######################### METHODS
